ed57048ba203be96126d317eb7b0c2b793cabda9
4hurs <fawstainer@gmail.com>
2025-10-30 01:23:54 +0200

    update process.php

--- Лабораторная работа №4 (штраф) переделка/nginx-lab/www/process.php
+++ Лабораторная работа №4 (штраф) переделка/nginx-lab/www/process.php
@@ -1,6 +1,9 @@
 <?php
 session_start();
 
+require_once 'ApiClient.php';
+require_once 'UserInfo.php';
+
 $full_name = htmlspecialchars(trim($_POST['full_name'] ?? ''));
 $age = htmlspecialchars(trim($_POST['age'] ?? ''));
 $course = htmlspecialchars(trim($_POST['course'] ?? ''));
@@ -31,6 +34,26 @@ setcookie('age', $age, time() + (30 * 24 * 60 * 60));
 setcookie('course', $course, time() + (30 * 24 * 60 * 60));
 setcookie('payment', $payment, time() + (30 * 24 * 60 * 60));
 setcookie('certificate', $certificate, time() + (30 * 24 * 60 * 60));
+
+// Кука времени последней отправки
+setcookie("last_submission", date('Y-m-d H:i:s'), time() + 3600, "/");
+
+// Получаем данные из API GitHub Topics с кешированием
+$api = new ApiClient();
+$cacheFile = 'api_cache.json';
+$cacheTtl = 300; // 5 минут
+
+if (file_exists($cacheFile) && time() - filemtime($cacheFile) < $cacheTtl) {
+    $cached = json_decode(file_get_contents($cacheFile), true);
+    $_SESSION['api_data'] = $cached;
+} else {
+    $apiData = $api->request('https://api.github.com/topics');
+    file_put_contents($cacheFile, json_encode($apiData, JSON_UNESCAPED_UNICODE));
+    $_SESSION['api_data'] = $apiData;
+}
+
+// Сохраняем информацию о пользователе
+$_SESSION['user_info'] = UserInfo::getInfo();
 
 // Сохраняем данные в файл data.txt
 $line = $full_name . ";" . $age . ";" . $course . ";" . $payment . ";" . $certificate . ";" . date('Y-m-d H:i:s') . "\n";
